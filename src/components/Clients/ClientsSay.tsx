f726d674fba8369d9399c2f7ecb7a3359a0edb31
Your Name <ronaldotieno13225@gmail.com>
2024-07-23 08:44:42 +0300

    first commit

--- src/components/Clients/ClientsSay.tsx
+++ src/components/Clients/ClientsSay.tsx
@@ -1,18 +1,26 @@
+import Image from 'next/image';
+
 export default function ClientsSay() {
     return (
         <>
-            <div className="text-center flex-col justify-center my-[10%] md:mx-[10%] lg:mx-[25%]">
+            <div className="text-center flex flex-col justify-center my-[10%] md:mx-[10%] lg:mx-[25%]">
                 <h1 className="text-2xl mb-9 font-semibold">Testimonial</h1>
-                <h1 className="md:text-6xl text-5xl ">What Our Clients Say</h1>
-               <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTxdZ-U0J-tnLvCTJerzztW49qGTlFgcHhrGrshmQUsoh7OW77Q" alt=""
-               style={{width: '100px', height: '80px', marginLeft:'45%'}}
-               className="my-9"  />
-                <p className="text-xl text-gray-500 ">Lorem ipsum dolor sit amet
-                    consectetur adipisicing elit. Aut architecto a molestias itaque porro accusantium ipsam atque iure
-                    deleniti tempore dignissimos corrupti autem, maiores maxime, facilis 
+                <h1 className="md:text-6xl text-5xl mb-6">What Our Clients Say</h1>
+                <div className="flex justify-center mb-9">
+                    <Image 
+                        src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTxdZ-U0J-tnLvCTJerzztW49qGTlFgcHhrGrshmQUsoh7OW77Q" 
+                        alt="Client testimonial" 
+                        width={100} 
+                        height={80} 
+                        className="object-cover"
+                    />
+                </div>
+                <p className="text-xl text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut architecto a molestias itaque porro 
+                    accusantium ipsam atque iure deleniti tempore dignissimos corrupti autem, maiores maxime, facilis 
                     recusandae neque pariatur eos expedita.
                 </p>
             </div>
         </>
-    )
+    );
 }
