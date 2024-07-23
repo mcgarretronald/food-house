a77ab4ab21030a3c1c5d37adfe2ab8b3405f9f3c
Your Name <ronaldotieno13225@gmail.com>
2024-07-23 08:50:11 +0300

    first commit

--- src/components/PopularFoods/PopularFoods.tsx
+++ src/components/PopularFoods/PopularFoods.tsx
@@ -1,19 +1,25 @@
-import Popularfood from "./Popularfood"
+import Image from 'next/image';
+import Popularfood from './Popularfood';
 
 export default function PopularFoods() {
     return (
         <>
-            <h1 className="text-4xl text-center  mt-3.5 font-black">Online Store</h1>
-            <h1 className="text-6xl text-center">Popular Foods</h1>
-            <img
-                src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS5DjSwYaA09cYJuCGUno6mihbMKo6W9qYcJoa1Jm25L-IKtTpF"
-                alt="Popular Food"
-                className=" w-96 mx-auto md:w-1/2 md:mx-96 mt-5"
-                style={{ width: "250px" }}
-            />
+            <h1 className="text-4xl text-center mt-3.5 font-black">Online Store</h1>
+            <h1 className="text-6xl text-center mb-5">Popular Foods</h1>
+            <div className="flex  mt-5 relative">
+                <Image 
+                    src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS5DjSwYaA09cYJuCGUno6mihbMKo6W9qYcJoa1Jm25L-IKtTpF" 
+                    alt="Popular Food" 
+                    width={250} 
+                    height={250} 
+                    className="object-cover lg:ml-[30%] ml-5"
+
+
+                />
+            </div>
             <div>
                 <Popularfood />
             </div>
         </>
-    )
+    );
 }
